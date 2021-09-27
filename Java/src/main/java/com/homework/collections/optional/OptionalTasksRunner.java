683943e0ebefd18931c4ea9b4a684924c186067f
in7hesky <anton.mayko.qa@gmail.com>
2021-09-27 19:40:36 +0300

    addition

--- Java/src/main/java/com/homework/collections/optional/OptionalTasksRunner.java
+++ Java/src/main/java/com/homework/collections/optional/OptionalTasksRunner.java
@@ -75,4 +75,96 @@ public class OptionalTasksRunner {
         System.out.println("UNIQUE_WORDS:");
         uniqueWords.forEach(System.out::println);
     }
+
+    // Написать метод isPalindrome, приниммающий int number, возвращающий boolean. Число Palindrome
+    // в обратном порядке равно исходному ( 1221 )
+    public static boolean isPalindrome(int number) {
+
+        String stringNumber = "" + number;
+        char[] charArray = stringNumber.toCharArray();
+        String reversedString = "";
+
+        for (int i = charArray.length - 1; i >=0 ; i--) {
+            reversedString += charArray[i];
+        }
+        if (number < 0) {
+            reversedString = "-" + reversedString.substring(0, reversedString.length() - 1);
+        }
+
+        int reversedNumber = Integer.parseInt(reversedString);
+        return number == reversedNumber;
+    }
+
+    // Написать метод, принимающий число и выводящий цифры числа поименно
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int reversedNumber = reverse(number);
+        int digitCount = getDigitCount(number);
+        int reversedLength = Integer.toString(reversedNumber).length();
+
+        int startingZeroes = digitCount - reversedLength;
+
+
+        for (int i = 0, current = 0; i < reversedLength; i++) {
+            current = reversedNumber % 10;
+            reversedNumber /= 10;
+
+            String comment;
+            switch (current) {
+                case 1:
+                    comment = "One";
+                    break;
+                case 2:
+                    comment = "Two";
+                    break;
+                case 3:
+                    comment = "Three";
+                    break;
+                case 4:
+                    comment = "Four";
+                    break;
+                case 5:
+                    comment = "Five";
+                    break;
+                case 6:
+                    comment = "Six";
+                    break;
+                case 7:
+                    comment = "Seven";
+                    break;
+                case 8:
+                    comment = "Eight";
+                    break;
+                case 9:
+                    comment = "Nine";
+                    break;
+                default:
+                    comment = "Zero";
+                    break;
+            }
+
+            System.out.println(comment);
+        }
+        for (int i = 0; i < startingZeroes; i++) {
+            System.out.println("Zero");
+        }
+    }
+
+    public static int reverse(int number) {
+        String reversed = new StringBuilder(Integer.toString(number)).reverse().toString();
+        if (number < 0) {
+            reversed = "-" + reversed.substring(0, reversed.length() - 1);
+        }
+        return Integer.parseInt(reversed);
+    }
+
+    public static int getDigitCount(int number) {
+        if(number < 0)
+            return -1;
+        return Integer.toString(number).length();
+    }
 }
